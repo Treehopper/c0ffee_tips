314943b159c5e70adccf0005f8460dab00e1839a
Max Hohenegger <github.com@hohenegger.eu>
2016-02-23 01:36:00 +0100

    Improved hover conversion
    - do not require selection but use white space separated words
    - also convert strings

--- eu.hohenegger.c0ffee_tips.ui/src/eu/hohenegger/c0ffee_tips/ui/impl/ConverterTextHover.java
+++ eu.hohenegger.c0ffee_tips.ui/src/eu/hohenegger/c0ffee_tips/ui/impl/ConverterTextHover.java
@@ -4,7 +4,6 @@ import org.eclipse.jdt.ui.text.java.hover.IJavaEditorTextHover;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorPart;
 
@@ -16,14 +15,16 @@ public class ConverterTextHover implements /*ITextHoverExtension2, ITextHoverExt
 	public String getHoverInfo(ITextViewer textViewer, IRegion hoverRegion) {
 		String[] result = new String[1];
 		Display.getDefault().syncExec(() -> {
-			Point selectedRange = textViewer.getSelectedRange();
 			try {
 				String tip = null;
-				String string = textViewer.getDocument().get(selectedRange.x, selectedRange.y);
+				String string = textViewer.getDocument().get(hoverRegion.getOffset(), hoverRegion.getLength());;
+				if (string.startsWith("\"") && string.endsWith("\"")) {
+					string = string.substring(1, string.length() - 1);
+				}
 				if (string.startsWith("0x")) {
 					tip = ConverterUtil.convert(string);
 				}
-				result[0] = tip;
+				result[0] = String.format("%s=%s", string, tip);
 			} catch (BadLocationException e) {
 				result[0] = null;
 			}
